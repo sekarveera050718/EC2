7f1b6186a378455c3cd151204779702be3f095b5
maheshmadmax <30284457+maheshmadmax@users.noreply.github.com>
2020-10-21 16:07:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,13 +6,7 @@ resource "aws_instance" "Ubuntu" {
 	ami = "ami-03f0fd1a2ba530e75"
 	instance_type = "t2.micro"
 	key_name = "demokey"
-	user_data = << EOF
-		#! /bin/bash
-		sudo apt-get update
-		sudo apt install -y default-jre
-		sudo apt install -y openjdk-11-jre-headless
-		sudo apt install -y openjdk-8-jre-headless
-    EOF
+	user_data = "${file("install_java.sh")}"
 	tags = {
 		Name = "Jenkins-EC2-Java"	
 		}
