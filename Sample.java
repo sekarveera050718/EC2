37ca715e7356abf1714d2704e545cff3b6fac1f0
maheshmadmax <30284457+maheshmadmax@users.noreply.github.com>
2020-10-22 09:23:36 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,22 @@
+/**
+ * Originally posted at: (not available now)
+ * http://blogs.sun.com/andreas/resource/InstallCert.java
+ * Use:
+ * java InstallCert hostname
+ * Example:
+ *% java InstallCert ecc.fedora.redhat.com
+ */
+
+
+import java.io.*;
+
+/**
+ * Class used to add the server's certificate to the KeyStore
+ * with your trusted certificates.
+ */
+public class Sample {
+  public static void main(String[] args){
+    System.out.println("This is my first program in java");
+  }//End of main
+
+}
